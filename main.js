ac6a6cd5bf32b9d3353e1b6d32e8dfe5b5ec129d
Andy Polhill <apolhill@gmail.com>
2014-12-10 08:56:01 +0000

    First tab example

---
+++ main.js
@@ -0,0 +1,8 @@
+requirejs.config({
+  baseUrl: './monkeyfucker',
+  paths: {
+    lib: 'bower_components/accessible-tabs/lib',
+    flight: 'bower_components/flight',
+    jquery: 'bower_components/jquery/dist/jquery'
+  }
+});
